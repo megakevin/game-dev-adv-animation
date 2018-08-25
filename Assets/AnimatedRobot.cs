5753f19c1377d6a09ee3ac58f181297016c4aee0
Kevin Campusano <megakevinx@gmail.com>
2018-08-25 15:17:21 -0400

    initial commit

---
+++ Assets/AnimatedRobot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedRobot : MonoBehaviour {
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void Jump()
+    {
+        this.animator.SetTrigger("JumpTrigger");
+    }
+}
